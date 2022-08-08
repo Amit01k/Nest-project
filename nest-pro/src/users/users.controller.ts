5043bb911826fc8a503b3d817734abcfa4e9beb8
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-08-08 12:57:55 +0530

    post method working

--- nest-pro/src/users/users.controller.ts
+++ nest-pro/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Post } from '@nestjs/common';
 
 
 @Controller('users')
@@ -23,6 +23,12 @@ export class UsersController {
 
         }
     }
+    @Post("add-user")
+        postFun():string{
+
+            return "hii how are you brothers"
+        }
+    
 }
 
 
