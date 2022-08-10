a4db2d80276d753432d16f4a10480dd679addfd1
Amit Kumar10 <amit.kumar10@olacabs.com>
2022-08-10 11:55:29 +0530

    gpg keys

--- nest-pro/src/posts/posts.controller.ts
+++ nest-pro/src/posts/posts.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { pipeline } from 'stream';
 
 @Controller('posts')
 export class PostsController {
@@ -10,5 +21,17 @@ export class PostsController {
   @HttpCode(200)
   postHello() {
     return 'this is coming from post controller form post method';
+  }
+  @Post('post-pipe/:id')
+  @HttpCode(200)
+  pipeMessage(@Param('id', new ParseIntPipe({errorHttpStatusCode:HttpStatus.NOT_ACCEPTABLE})) data: any) {
+    //here we add parseIntpipe who accept only integer input and we put some error msg by this way
+    return 'this is params id' + data;
+  }
+  @Post('post-pipe-page/:id') 
+  @HttpCode(200)
+  pipeMessagePage(@Query('page',new DefaultValuePipe(10))page:number):String {
+    //here we add parseIntpipe who accept only integer input and we put some error msg by this way
+    return 'this is params id' + page;
   }
 }
