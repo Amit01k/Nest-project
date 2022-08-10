e21c447c0b8616934971c4bef7e0c4165ec8d70d
Amit01 <amitkumar1437786@gmail.com>
2022-08-10 12:25:06 +0530

    gpg keys added

--- nest-pro/src/posts/posts.controller.ts
+++ nest-pro/src/posts/posts.controller.ts
@@ -24,14 +24,25 @@ export class PostsController {
   }
   @Post('post-pipe/:id')
   @HttpCode(200)
-  pipeMessage(@Param('id', new ParseIntPipe({errorHttpStatusCode:HttpStatus.NOT_ACCEPTABLE})) data: any) {
+  pipeMessage(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    data: any,
+  ) {
     //here we add parseIntpipe who accept only integer input and we put some error msg by this way
     return 'this is params id' + data;
   }
-  @Post('post-pipe-page/:id') 
+  @Post('post-pipe-page/:id')
   @HttpCode(200)
-  pipeMessagePage(@Query('page',new DefaultValuePipe(10))page:number):String {
+  pipeMessagePage(
+    @Query('page', new DefaultValuePipe(10)) page: number,
+  ): String {
     //here we add parseIntpipe who accept only integer input and we put some error msg by this way
     return 'this is params id' + page;
   }
+
+  //dsuhfughdsiufhgiohdfsoihgidfhiogshiofdhigdsfhgisdfhigodsiohgoifdigiodhb
+  //adding pgpg keys
 }
